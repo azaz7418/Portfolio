4255db33e09d126c77c95e13052036b4e9e1789c
azaz7418 <wazaz7418@gmail.com>
2024-10-24 01:12:33 +0600

    resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,23 @@ import { FiDownload } from "react-icons/fi";
 import Social from "./Social";
 import Image from "./Image";
 import States from "../components/States";
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 
 const Home = () => {
+  
+  const handleDownloadPdf = () => {
+    const content = contentRef.current;
+    html2canvas(content, { scale: 2 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4");
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save("Resume.pdf");
+    });
+  };
   return (
     <section>
       <div className="container mx-auto">
@@ -24,7 +39,7 @@ const Home = () => {
             {/* Button and Social */}
             <div className=" flex flex-col md:flex-row xl:flex-row items-center gap-8">
               <button className=" button outline-0 hover:text-primary bg-transparent border border-solid border-spacing-1 border-accent text-accent uppercase flex gap-2">
-                <span className="">Download CV</span>
+                <span onClick={handleDownloadPdf} className="">Download CV</span>
                 <FiDownload className=" text-xl" />
               </button>
               <div className=" mb-8 xl:mb-0 md:mb-0">
