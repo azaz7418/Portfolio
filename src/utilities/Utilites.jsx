e0d673d106daadfdda6016008f346d95fbc04eae
azaz7418 <wazaz7418@gmail.com>
2025-07-11 17:54:41 +0600

    new resume

--- src/utilities/Utilites.jsx
+++ src/utilities/Utilites.jsx
@@ -1,7 +1,7 @@
 
 export const downloadFile = (pdf) => {
     const fileUrl = pdf; 
-    const fileName = "resume.pdf"; 
+    const fileName = "Azaz_Ahamed_Resume.pdf"; 
     const anchor = document.createElement("a");
     anchor.href = fileUrl;
     anchor.download = fileName;
